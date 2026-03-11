880328ae91c0fe3ca04348c67fdcf879b13c39d4
jodielmclaughlin <jodieleighmclaughlin@gmail.com>
2026-03-11 12:02:25 +0000

    trialling assigning return value of a method to a variable

--- CSBasics/Program.cs
+++ CSBasics/Program.cs
@@ -12,12 +12,22 @@ namespace CSBasics
             Console.WriteLine(isStringLonger("My name is Jodie", 6));
             Console.WriteLine(isStringLonger("Whats for lunch?", 22));
             Console.WriteLine(isStringLonger("Nice, love that", 2));
+
+
+            int additionResult = Sum(10, 5);
+
+            Console.WriteLine(additionResult);
         }
         private static bool isStringLonger(string word, int number)
         {
             
             return word.Length > number;
          
+        }
+
+        private static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
         }
     }
 }
