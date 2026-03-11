7c4c3a8dc3af201a8ccaaf1f195aaad7a8acf2a1
jodielmclaughlin <jodieleighmclaughlin@gmail.com>
2026-03-11 11:54:37 +0000

    completed bool sprint task

--- CSBasics/Program.cs
+++ CSBasics/Program.cs
@@ -7,13 +7,16 @@ namespace CSBasics
         static void Main(string[] args)
         {
             
-            Console.WriteLine(subtractNums(540,423));
-
+            Console.WriteLine(isStringLonger("Hi there",8));
+            Console.WriteLine(isStringLonger("Hello", 17));
+            Console.WriteLine(isStringLonger("My name is Jodie", 6));
+            Console.WriteLine(isStringLonger("Whats for lunch?", 22));
+            Console.WriteLine(isStringLonger("Nice, love that", 2));
         }
-        private static int subtractNums(int numOne, int numTwo)
+        private static bool isStringLonger(string word, int number)
         {
             
-            return numOne - numTwo;
+            return word.Length > number;
          
         }
     }
