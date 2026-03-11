2ecf213ff767c7775b2ebbe46c65eb41b95f3009
jodielmclaughlin <jodieleighmclaughlin@gmail.com>
2026-03-11 11:48:43 +0000

    completed int sprint task

--- CSBasics/Program.cs
+++ CSBasics/Program.cs
@@ -6,23 +6,15 @@ namespace CSBasics
     {
         static void Main(string[] args)
         {
-            //double pi = 3.14;
-            //Console.WriteLine(pi);
-            //bool isLearningCS = true;
-            //Console.WriteLine(isLearningCS);
-            //string favouriteFilm = "The Martian";
-            //Console.WriteLine("My favourite film is " + favouriteFilm);
-            //favouriteFilm = "High School Musical";
-            //Console.WriteLine("My favourite film is " + favouriteFilm);
-
-            string firstName = "Jodie";
-
-            printGreeting(firstName);
+            
+            Console.WriteLine(subtractNums(540,423));
 
         }
-        private static void printGreeting(string name)
+        private static int subtractNums(int numOne, int numTwo)
         {
-            Console.WriteLine("Hello, " + name);
+            
+            return numOne - numTwo;
+         
         }
     }
 }
