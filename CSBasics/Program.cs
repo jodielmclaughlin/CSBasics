ab51c24d7e7ef20c5a5464d27bce467a53e43475
jodielmclaughlin <jodieleighmclaughlin@gmail.com>
2026-03-11 11:40:54 +0000

    first commit

---
+++ CSBasics/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.NetworkInformation;
+
+namespace CSBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //double pi = 3.14;
+            //Console.WriteLine(pi);
+            //bool isLearningCS = true;
+            //Console.WriteLine(isLearningCS);
+            //string favouriteFilm = "The Martian";
+            //Console.WriteLine("My favourite film is " + favouriteFilm);
+            //favouriteFilm = "High School Musical";
+            //Console.WriteLine("My favourite film is " + favouriteFilm);
+
+            string firstName = "Jodie";
+
+            printGreeting(firstName);
+
+        }
+        private static void printGreeting(string name)
+        {
+            Console.WriteLine("Hello, " + name);
+        }
+    }
+}
